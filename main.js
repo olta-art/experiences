63659ee88baf0139a435f8e8aa84410aa62113c3
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-07-26 21:33:29 +0100

    innit

---
+++ main.js
@@ -0,0 +1,313 @@
+import { Details, Viewer } from "./o.js"
+import { getProjects } from "./querys.js"
+import { queryfetcher, SEPARATOR as sep, decode } from "./helpers.js"
+
+if ("customElements" in window) {
+  customElements.define("o-viewer", Viewer)
+  customElements.define("o-details", Details)
+}
+
+let projects = []
+
+let seed = 1
+let current = 0
+
+let projectInterval
+let editionInterval
+
+const options = {
+  projects: [],
+  timing: {
+    project: 90,
+    edition: 30
+  },
+  display: {
+    name: true,
+    creator: true,
+    description: false,
+    qr: true,
+  }
+}
+
+// viewer
+const viewer = document.createElement("o-viewer")
+viewer.setAttribute("url", "")
+viewer.classList = "o-viewer"
+document.body.appendChild(viewer)
+
+// details
+const details = document.createElement("o-details")
+details.classList = "o-details"
+document.body.appendChild(details)
+
+// random button
+const random = document.querySelector(".random")
+random.addEventListener("click", () => {
+  seed = getRandSeed(seed, currentProject().editionSize)
+  viewer.setAttribute("url", getUrl())
+  random.classList = "random spin"
+  disableButtons()
+})
+
+// change button
+const change = document.querySelector(".change")
+change.addEventListener("click", () => {
+  current = incrementLoop(current, options.projects.length)
+  seed = getRandSeed(seed, currentProject().editionSize)
+
+  isSeeded() ? random.removeAttribute("hidden") : random.setAttribute("hidden", "")
+
+  viewer.setAttribute("url", getUrl())
+  change.classList = "change spin"
+  disableButtons()
+})
+
+const optionsEl = document.querySelector("dialog.options")
+const optionsForm = optionsEl.querySelector("#options-form")
+const closeButton = optionsEl.querySelector("button.close")
+closeButton.addEventListener("click", () => optionsEl.close())
+document.addEventListener("keyup", ({ key }) => {
+  o
+  if (key.toLowerCase() === "o") {
+    optionsEl.hasAttribute("open") ? optionsEl.close() : optionsEl.showModal()
+  }
+})
+
+
+optionsForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const f = new FormData(e.target)
+
+  // Display the key/value pairs
+  for (const [k, v] of f.entries()) {
+    console.log(`${k}, ${v}`)
+  }
+})
+
+optionsForm.addEventListener("change", (e) => {
+  console.log(e.target.value, e.target.checked, e.target.name, e.target.closest("fieldset").name)
+  const field = e.target.closest("fieldset").name
+  const name = e.target.name
+  const v = e.target.value == "on" ? e.target.checked : e.target.value
+
+  switch (field) {
+    case "projects":
+      const i = options.projects.findIndex(p => p == name)
+      if (i == -1 && v) {
+        options.projects.push(name)
+      } else if ((i == 0 || i) && !v) {
+        options.projects.splice(i, 1)
+      }
+      break
+
+    case "timing":
+      options.timing[name] = parseInt(v)
+      // update
+      if (name == "project") {
+        updateProjectInterval(v)
+      }
+
+      if (name == "edition") {
+        updateEditionInterval(v)
+      }
+
+      break
+
+    case "display":
+      options.display[name] = v
+      // update
+      const el = details.shadowRoot.querySelector(`slot[name="${name}"]`)
+      v ? el.removeAttribute("hidden") : el.setAttribute("hidden", true)
+      break
+
+    default:
+      break
+  }
+})
+
+function renderOptions() {
+  let projectOptions = []
+  for (const p of projects) {
+    projectOptions.push(`
+      <div>
+        <input name="${p.id}" type="checkbox" id="${p.id}" checked>
+        <label for="${p.id}">${p.name} | ${p.creator.profile.name}</label>
+      </div>
+    `)
+  }
+
+  optionsForm.innerHTML = `
+    <fieldset name="projects">
+      <legend>Projects</legend>
+      ${projectOptions.join("") ?? ""}
+    </fieldset>
+
+    <fieldset name="timing">
+    <legend>Timing</legend>
+      <div>
+        <label for="project-time">Project Time</label>
+        <input name="project" type="number" min="10" id="project-time" value="90">
+        <small>seconds</small>
+      </div>
+      <div>
+        <label for="edition-time">Edition Time</label>
+        <input name="edition" type="number" min="5" id="edition-time" value="30">
+        <small>seconds</small>
+      </div>
+    </fieldset>
+
+    <fieldset name="display">
+    <legend>Display</legend>
+      <div>
+        <input name="name" type="checkbox" id="project-name" checked>
+        <label for="project-name">name</label>
+      </div>
+      <div>
+        <input name="creator" type="checkbox" id="project-creator" checked>
+        <label for="project-creator">creator</label>
+      </div>
+      <div>
+        <input name="description" type="checkbox" id="project-description" checked>
+        <label for="project-description">description</label>
+      </div>
+      <div>
+        <input name="qrcode" type="checkbox" id="project-qr" checked>
+        <label for="project-qr">qr code</label>
+      </div>
+    </fieldset>
+  `
+
+  updateProjectInterval(90)
+  updateEditionInterval(30)
+}
+
+function updateProjectInterval(seconds) {
+  if (projectInterval) {
+    clearInterval(projectInterval)
+  }
+  projectInterval = setInterval(() => {
+    colorTrace("FUCKKKK", "orange")
+    current = incrementLoop(current, options.projects.length)
+    seed = getRandSeed(seed, currentProject().editionSize)
+
+    isSeeded() ? random.removeAttribute("hidden") : random.setAttribute("hidden", "")
+
+    viewer.setAttribute("url", getUrl())
+    change.classList = "change spin"
+    disableButtons()
+  }, seconds * 1000)
+
+  updateEditionInterval(options.timing.edition)
+}
+
+function updateEditionInterval(seconds) {
+  if (editionInterval) {
+    clearInterval(editionInterval)
+  }
+  editionInterval = setInterval(() => {
+    seed = getRandSeed(seed, currentProject().editionSize)
+    viewer.setAttribute("url", getUrl())
+    random.classList = "random spin"
+    disableButtons()
+  }, seconds * 1000)
+}
+
+const spinner = document.querySelector(".spinner")
+
+function disableButtons() {
+  // TODO: show spinner
+  random.setAttribute("disabled", "")
+  change.setAttribute("disabled", "")
+  setTimeout(() => {
+    change.removeAttribute("disabled")
+    random.removeAttribute("disabled")
+
+    change.classList = "change"
+    random.classList = "random"
+
+    const { name, description, creator } = getDetails()
+    console.log("olta", name)
+    details.setAttribute("name", name)
+    details.setAttribute("description", decode(description))
+    details.setAttribute("creator", creator)
+    details.setAttribute("qrcode", `https://olta.art/project/${currentProject().id}`)
+  }, 2500)
+}
+
+function incrementLoop(current, length) {
+  const next = current + 1
+  if (next >= length) return 0
+
+  return next
+}
+
+function getRandSeed(seed, editionSize) {
+  if (editionSize == 1) return "1"
+
+  const s = Math.floor(Math.random() * editionSize) + 1
+  if (seed === s) {
+    return getRandSeed(seed, editionSize)
+  } else {
+    return s
+  }
+}
+
+function getUrl() {
+  if (!projects || projects.length == 0) return
+
+  const url = currentProject().lastAddedVersion.animation.url
+    .replace("http:", "https:")
+
+  const address = currentProject().id
+
+  return `${url}?id=1&seed=${seed}&address=${address}`
+}
+
+function isSeeded() {
+  return currentProject().editionSize > 1
+}
+
+function getDetails() {
+  if (!projects || projects.length == 0) return
+
+  console.log(currentProject())
+  const { name, description, creator } = currentProject()
+
+  return {
+    name,
+    description,
+    creator: creator.profile.name
+  }
+}
+
+function currentProject() {
+  const id = options.projects[current]
+  console.log("PROJECT", current, id)
+  const proj = projects.find(p => p.id == id)
+  console.log(proj)
+  return proj
+}
+
+; (async function fetchProjects() {
+  const resp = await queryfetcher(
+    "https://api.thegraph.com/subgraphs/name/olta-art/polygon-v1",
+    getProjects()
+  )
+
+  optionsEl.showModal()
+
+  projects = [...resp.projects]
+  options.projects = projects.map(p => p.id)
+
+  renderOptions()
+
+  seed = getRandSeed(seed, projects[current].editionSize)
+  viewer.setAttribute("url", getUrl())
+  disableButtons()
+})()
+
+function colorTrace(msg, color) {
+  console.log("%c" + msg, "color:" + color + ";font-weight:bold;")
+}
+
+
---
+++ o.js
@@ -0,0 +1,119 @@
+import { SEPARATOR as sep, ranger, queryfetcher } from "./helpers.js"
+
+export class Viewer extends HTMLElement {
+
+  // Specify observed attributes so that
+  // attributeChangedCallback will work
+  static get observedAttributes() {
+    return ["url"]
+  }
+
+  constructor(options) {
+    super()
+
+    console.log(options)
+
+    const iframe = ranger(`
+      <iframe src="" sandbox="allow-scripts allow-same-origin"></iframe>
+    `)
+
+    const style = ranger(`
+    <style>
+      iframe {
+        transition: opacity 500ms;
+        grid-area: one;
+        width: calc(100% + 1em);
+        height: calc(100% + 1em);
+        border: none;
+      }
+    </style>
+  `)
+
+    const shadow = this.attachShadow({ mode: 'open' })
+    shadow.appendChild(style.cloneNode(true))
+    shadow.appendChild(iframe.cloneNode(true))
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(name, newValue)
+
+    if (name === "url" && newValue) {
+      const oldIframes = this.shadowRoot.querySelectorAll("iframe")
+      oldIframes.forEach(f => f.className = "old")
+
+      const template = ranger(`
+        <iframe
+          src="${newValue}"
+          style="opacity: 0.0;"
+          class="new"
+          sandbox="allow-scripts allow-same-origin"
+        />
+      `)
+
+      this.shadowRoot.appendChild(template.cloneNode(true))
+
+      // HACK: no way of knowing content loaded without post-message
+      setTimeout(() => {
+        const newIframe = this.shadowRoot.querySelector(".new")
+        console.log(newIframe)
+        newIframe.style = "opacity: 1;"
+
+        setTimeout(() => {
+          const oldIframes = this.shadowRoot.querySelectorAll(".old")
+          oldIframes.forEach(f => f.remove())
+        }, 500)
+
+      }, 2000)
+    }
+    // updateStyle(this);
+  }
+}
+
+export class Details extends HTMLElement {
+
+  // Specify observed attributes so that
+  // attributeChangedCallback will work
+  static get observedAttributes() {
+    return ["name", "creator", "description", "qrcode"]
+  }
+
+  constructor() {
+    super()
+
+    const template = document.getElementById("o-details")
+
+    const shadow = this.attachShadow({ mode: 'open' })
+    shadow.appendChild(template.content.cloneNode(true))
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "name" && newValue) {
+      const slot = this.shadowRoot.querySelector('slot[name="name"]')
+      slot.textContent = newValue
+    }
+    if (name === "creator" && newValue) {
+      const slot = this.shadowRoot.querySelector('slot[name="creator"]')
+      slot.textContent = newValue
+    }
+    if (name === "description" && newValue) {
+      const slot = this.shadowRoot.querySelector('slot[name="description"]')
+      slot.textContent = newValue
+    }
+    if (name === "qrcode" && newValue) {
+      const slot = this.shadowRoot.querySelector('slot[name="qrcode"]')
+      const qrEl = document.createElement("div")
+      qrEl.classList.add("qrcode")
+      new QRCode(qrEl, {
+        text: newValue,
+        width: 128,
+        height: 128,
+      })
+
+      slot.innerHTML = ""
+      slot.append(qrEl)
+    }
+  }
+}
+
+
+
